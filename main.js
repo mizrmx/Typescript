d735fb579ba6f545b6c3e12c6ef49b913a30fd5e
Mayra Zurita <mizrmx@gmail.com>
2021-10-12 17:03:26 -0500

    Agregando archivos

--- main.js
+++ main.js
@@ -1,49 +1,14 @@
 "use strict";
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: [1, 2]
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    let superman = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: [1],
-        getName() {
-            return this.name;
-        }
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf()))
+            ? false
+            : true;
     };
-})();
-(() => {
-    const client = {
-        name: 'Fernando',
-        age: 25,
-        address: {
-            id: 125,
-            zip: '86720',
-            city: 'Tabasco'
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-    const client2 = {
-        name: 'Melissa',
-        age: 30,
-        address: {
-            id: 125,
-            zip: '86720',
-            city: 'Tabasco'
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-})();
-(() => {
-    let addNumbersFunction;
-    addNumbersFunction = (a, b) => {
-        return 10;
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('Mayra'));
 //# sourceMappingURL=main.js.map
