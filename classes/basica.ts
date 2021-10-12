8c513e614e89ec17c9a9bd4117a8407fb906b86a
Mayra Zurita <mizrmx@gmail.com>
2021-10-12 11:23:05 -0500

    update

--- classes/basica.ts
+++ classes/basica.ts
@@ -6,17 +6,24 @@
         // private team: string;
         // public realName?: string;
         static age: number = 35;
+        static getAvgAge() {
+            return this.name;
+        }
 
         constructor( 
             private name: string, 
             private team: string, 
             public realName?: string 
         ) {}
+
+        public bio() {
+            return `${ this.name } (${this.team})`
+        }
     }
 
 
     const antman: Avenger = new Avenger ('Antman', 'Capitan', 'Scott Lang');
     console.log( antman)
 
-    console.log(Avenger.age)
+    console.log( Avenger.getAvgAge ())
 })()
--- main.js
+++ main.js
@@ -6,10 +6,16 @@
             this.team = team;
             this.realName = realName;
         }
+        static getAvgAge() {
+            return this.name;
+        }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
     Avenger.age = 35;
     const antman = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log(antman);
-    console.log(Avenger.age);
+    console.log(Avenger.getAvgAge());
 })();
 //# sourceMappingURL=main.js.map
