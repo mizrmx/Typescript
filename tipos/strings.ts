8a7b6cbf4dde60d30c04e9784f234b629dd9d40a
Mayra Zurita <mizrmx@gmail.com>
2021-10-21 14:53:10 -0500

    Agregando archivos

---
+++ tipos/strings.ts
@@ -0,0 +1,10 @@
+(()=> {
+
+    const batman: string = 'Batman';
+    const linternaVerde:string = "Linterna Verde";
+    const volvanNegro:string = `Héroe: Volcan Negro`;
+
+    console.log(` I'm ${ batman }`)
+
+    console.log( batman.toUpperCase() )
+})()
