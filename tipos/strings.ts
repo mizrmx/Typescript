6fee62689dde96ee2310741b319e52ce94d4391f
Mayra Zurita <mizrmx@gmail.com>
2021-10-21 15:32:49 -0500

    Agregando generics

--- tipos/strings.ts
+++ tipos/strings.ts
@@ -4,7 +4,8 @@
     const linternaVerde:string = "Linterna Verde";
     const volvanNegro:string = `Héroe: Volcan Negro`;
 
-    console.log(` I'm ${ batman }`)
+    console.log(` I'm ${ batman }`);
+    console.log( batman.toUpperCase() );
 
-    console.log( batman.toUpperCase() )
+    console.log( batman[10].toUpperCase() )
 })()
